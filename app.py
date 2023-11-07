2d587196194acb006b3204c9a1777fb26a1b4db2
안정진 <120768669+An-Jeong-Jin@users.noreply.github.com>
2023-11-08 02:41:31 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from time import sleep
+
+st.set_page_config(
+    page_icon= "🕶️",
+    page_title="지도",
+    layout="wide",    
+
+)
+
+st.header("AI 프로젝트 지도🗺️")
+st.subheader("지도🗺️🗺️🗺️🗺️🗺️")
